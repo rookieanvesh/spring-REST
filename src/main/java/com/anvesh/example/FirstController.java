7478333bfbaabe0471846a2157e5c877cbe5279d
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-09-25 23:36:02 +0530

    post

--- src/main/java/com/anvesh/example/FirstController.java
+++ src/main/java/com/anvesh/example/FirstController.java
@@ -1,8 +1,7 @@
 package com.anvesh.example;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +10,15 @@ public class FirstController {
     public String sayHello(){
         return "hello from 1st controller";
     }
+/*
     @GetMapping("/hello-2")
     @ResponseStatus(HttpStatus.ACCEPTED)//this is an enum class which has http objects and gives us status codes, by convention we should use this
     public String sayHello2(){
         return "hello from 2nd controller";
+    }
+*/
+    @PostMapping("/post")
+    public String post(String message){//in order to send a message we give it a body message
+        return "request accepted and message" + message;
     }
 }
