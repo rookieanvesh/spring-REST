7d9170784465a47318da4cea04bc423e193dae58
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-09-27 18:46:07 +0530

    JsonProperty

--- src/main/java/com/anvesh/example/Order.java
+++ src/main/java/com/anvesh/example/Order.java
@@ -1,7 +1,45 @@
 package com.anvesh.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Order {
+    @JsonProperty("c-name")
     private String customerName;
+    @JsonProperty("p-name")
     private String productName;
+    @JsonProperty("quantity")
     private int quantity;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "customerName='" + customerName + '\'' +
+                ", productName='" + productName + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
